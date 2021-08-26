432c78eaedd592c134cf9dea05bcf05b9734d76b
Jayson Infante <jayson.infante708@gmail.com>
2021-08-26 13:34:34 -0400

    Fixed smooth scroll

--- models.js
+++ models.js
@@ -1,35 +1,4 @@
-// import './style.css'
-
-// import * as THREE from 'three';
 import * as THREE from "./node_modules/three/build/three.module.js";
-
-// import headshot from '/assets/Headshot.jpg?url';
-
-// console.log(headshot);
-
-// import longboard from '/assets/Square1/longboard.png?url';
-// import crunch from '/assets/Square1/crunch.png?url';
-// import acoustic from '/assets/Square1/acoustic.png?url';
-// import electric from '/assets/Square1/electric.png?url';
-// import music from '/assets/Square1/music.png?url';
-// import basketball from '/assets/Square1/basketball.png?url';
-
-// import bleach from '/assets/Square2/bleach.png?url';
-// import ca from '/assets/Square2/ca.png?url';
-// import compsci from '/assets/Square2/compsci.png?url';
-// import flag from '/assets/Square2/flag.png?url';
-// import sase from '/assets/Square2/sase.png?url';
-// import stevens from '/assets/Square2/stevens.png?url';
-
-// import ntc from '/assets/Square3/ntc.png?url';
-// import numerix from '/assets/Square3/numerix.png?url';
-// import pnc from '/assets/Square3/pnc.png?url';
-
-// import agile from '/assets/Square4/agile.png?url';
-// import card from '/assets/Square4/card.png?url';
-// import rutgers from '/assets/Square4/rutgers.png?url';
-// import transit from '/assets/Square4/transit.png?url';
-
 
 // Scene is like a container that holds all of our objects, cameras, and lights
 const scene = new THREE.Scene();
@@ -51,19 +20,12 @@ const renderer = new THREE.WebGLRenderer({
   alpha: true
 });
 
-// const renderer = new THREE.WebGLRenderer();
 
 // Set Pixel Ratio to window's device pixel ratio
 renderer.setPixelRatio( window.devicePixelRatio );
 
 // Make it a full screen size canvas
 renderer.setSize( window.innerWidth, window.innerHeight );
-
-// Camera is currently positioned at middle of scene so move it along z-axis
-// camera.position.setZ(30);
-// camera.position.setX(-3);
-
-// document.body.appendChild( renderer.domElement );
 
 // render method draws out our scene
 renderer.render( scene, camera );
@@ -75,20 +37,12 @@ renderer.render( scene, camera );
 */
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100);
 // Material usually requires a light source to bounce off of, but Basic Material doesn't
-// const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } );
 const material = new THREE.MeshStandardMaterial( { color: 0xff6347 } );
-const torus = new THREE.Mesh( geometry, material );
-
-// scene.add(torus);
 
 // Emits light in all directions 
 const pointLight = new THREE.PointLight(0xffffff);
 
-// Light is positioned inside of the torus
 pointLight.position.set(5,5,5);
-
-// Light is positioned farther away and lights up a larger area
-// pointLight.position.set(20,20,20);
 
 // Lighting across entire scene
 const ambientLight = new THREE.AmbientLight(0xffffff);
@@ -102,27 +56,7 @@ scene.add(pointLight, ambientLight);
 
 // const controls = new OrbitControls(camera, renderer.domElement);
 
-// Populate background with random shapes
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial( { color: 0xffffff } );
-  const star = new THREE.Mesh( geometry, material );
-
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
-
-  star.position.set(x, y, z);
-  scene.add(star);
-}
-
-// Array(200).fill().forEach(addStar);
-
-// Background
-
-// const spaceTexture = new THREE.TextureLoader().load('peterandrz.JPG');
-// scene.background = spaceTexture;
-
 // Avatar
-
 const jaysonTexture = new THREE.TextureLoader().load('/headshot.jpg');
 const jayson = new THREE.Mesh(
   new THREE.BoxGeometry(3,3,3),
@@ -131,8 +65,8 @@ const jayson = new THREE.Mesh(
 
 scene.add(jayson);
 
+// Cube 1 (Next to quote block)
 const quoteTexture = new THREE.TextureLoader();
-// quoteTexture.setPath('/assets/');
 const quoteArray = [
   new THREE.MeshBasicMaterial( { map: quoteTexture.load('/longboard.png') } ),
   new THREE.MeshBasicMaterial( { map: quoteTexture.load('/crunch.png') } ),
@@ -148,8 +82,8 @@ const quote = new THREE.Mesh(
 
 scene.add(quote);
 
+// Cube 2 (Next to About Me section)
 const aboutMeTexture = new THREE.TextureLoader();
-// aboutMeTexture.setPath('/assets/');
 const aboutMeArray = [
   new THREE.MeshBasicMaterial( { map: aboutMeTexture.load('/bleach.png') } ),
   new THREE.MeshBasicMaterial( { map: aboutMeTexture.load('/ca.png') } ),
@@ -166,7 +100,6 @@ const aboutMe = new THREE.Mesh(
 scene.add(aboutMe);
 
 // Work Square 1
-
 var workTexture1 = new THREE.PlaneGeometry(2,2,1,1);
 var workTexture2 = new THREE.PlaneGeometry(2,2,1,1);
 
@@ -188,7 +121,6 @@ workCardOne.add(mesh2);
 scene.add(workCardOne);
 
 // Work Square 2
-
 var workTexture3 = new THREE.PlaneGeometry(2,2,1,1);
 var workTexture4 = new THREE.PlaneGeometry(2,2,1,1);
 
@@ -210,7 +142,6 @@ workCardTwo.add(mesh4);
 scene.add(workCardTwo);
 
 // Project Card 1
-
 var projectTexture1 = new THREE.PlaneGeometry(2,2,1,1);
 var projectTexture2 = new THREE.PlaneGeometry(2,2,1,1);
 
@@ -232,7 +163,6 @@ projectCardOne.add(mesh2);
 scene.add(projectCardOne);
 
 // Project Card 2
-
 var projectTexture3 = new THREE.PlaneGeometry(2,2,1,1);
 var projectTexture4 = new THREE.PlaneGeometry(2,2,1,1);
 
@@ -253,19 +183,8 @@ projectCardTwo.add(mesh2);
 
 scene.add(projectCardTwo);
 
-// Moon
-// const hunnidTexture = new THREE.TextureLoader().load('100.png');
-// const hunnid = new THREE.Mesh(
-//   new THREE.SphereGeometry(3,32,32),
-//   new THREE.MeshStandardMaterial({
-//     map: hunnidTexture,
-//   })
-// );
 
-// scene.add(hunnid);
-
-// hunnid.position.z = 30;
-// hunnid.position.setX(-10);
+// Three.js object positioning
 
 jayson.position.z = -5;
 jayson.position.x = 3;
@@ -280,11 +199,11 @@ aboutMe.position.y = -9.5;
 
 workCardOne.position.z = -5;
 workCardOne.position.x = -5;
-workCardOne.position.y = -14.75;
+workCardOne.position.y = -15.1;
 
 workCardTwo.position.z = -5;
 workCardTwo.position.x = -5;
-workCardTwo.position.y = -16.75;
+workCardTwo.position.y = -17.1;
 
 projectCardOne.position.z = -5;
 projectCardOne.position.x = 5;
@@ -294,25 +213,16 @@ projectCardTwo.position.z = -5;
 projectCardTwo.position.x = 5;
 projectCardTwo.position.y = -24.3;
 
-var item2 = document.getElementById("item2");
-
 var mybutton = document.getElementById("myBtn");
 
 function moveCamera() {
   // How far we are from the top of the webpage
   const t = document.body.getBoundingClientRect().top;
 
-  // hunnid.rotation.x += 0.05;
-  // hunnid.rotation.y += 0.075;
-  // hunnid.rotation.z += 0.05;
-
   jayson.rotation.y += 0.01;
   jayson.rotation.z += 0.01;
 
-  // camera.position.z = t * -0.01;
-  // camera.position.x = t * -0.0002;
-  // camera.rotation.y = t * -0.0002;
-
+  // Needed so the three.js models appear to move in-line with the user scrolling
   camera.position.y = t * 0.008;
 
   if (document.documentElement.scrollTop > 20 || document.body.scrollTop > 20) {
@@ -326,22 +236,8 @@ document.body.onscroll = moveCamera;
 moveCamera();
 
 // Recursive function that gives infinite loop to call render automatically (Similar to game loop)
-
-// var mesh1x = 2.1;
-// var mesh1y = 3;
-
-// var mesh2x = 3;
-// var mesh2y = 3;
-
-
 function animate() {
   requestAnimationFrame( animate );
-
-  // torus.rotation.x += 0.01;
-  // torus.rotation.y += 0.005;
-  // torus.rotation.z += 0.01;
-
-  // hunnid.rotation.x += 0.005;
 
   jayson.rotation.y += 0.01;
   jayson.rotation.z += 0.01;
@@ -352,32 +248,11 @@ function animate() {
   aboutMe.rotation.y += 0.01;
   aboutMe.rotation.z += 0.01;
 
-  // workCardOne.rotation.x += 0.05;
   workCardOne.rotation.y += 0.02;
-  // workCardOne.rotation.z += 0.5;
-
-  // while (mesh1.scale.x < mesh1x){
-  //   mesh1.scale.x += 0.001;
-  //   mesh1.scale.y += 0.001;
-  //   mesh2.scale.x += 0.001;
-  //   mesh2.scale.y += 0.001;
-  // }
-
-  // while (mesh1.scale.x > 2){
-  //   mesh1.scale.x -= 0.001;
-  //   mesh1.scale.y -= 0.001;
-  //   mesh2.scale.x -= 0.001;
-  //   mesh2.scale.y -= 0.001;
-  // }
-
-  // workCardTwo.rotation.x += 0.05;
   workCardTwo.rotation.y += 0.02;
-  // workCardTwo.rotation.z += 0.5;
 
   projectCardOne.rotation.y += 0.02;
-
   projectCardTwo.rotation.y += 0.02;
-  // controls.update();
 
   renderer.render( scene, camera );
 }
